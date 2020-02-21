dd8315bedf10c7253499e66dc6ebcc60da9ae66e
David Speers <speerscd@gmail.com>
2020-02-21 15:43:33 +0100

    Add categories that lead to the game screen

---
+++ lib/screens/game.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GamePage extends StatefulWidget {
+  GamePage({Key key, @required this.category}) : super(key: key);
+
+  final String category;
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/select_category.dart
+++ lib/screens/select_category.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trends_with_friends/screens/game.dart';
 
 class SelectCategoryPage extends StatefulWidget {
   SelectCategoryPage({Key key}) : super(key: key);
@@ -10,6 +11,36 @@ class SelectCategoryPage extends StatefulWidget {
 class _SelectCategoryPageState extends State<SelectCategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Category'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.account_balance),
+            title: Text('Government'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GamePage(category: 'Government',)),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('Cryptocurrency'),
+          ),
+          ListTile(
+            leading: Icon(Icons.android),
+            title: Text('Technology'),
+          ),
+          ListTile(
+            leading: Icon(Icons.flare),
+            title: Text('Star Wars'),
+          ),
+        ],
+      ),
+    );
   }
 }
